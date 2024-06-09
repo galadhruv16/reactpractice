b2e1f08647e8183a0534ef21a00b846b74da0857
galadhruv16 <galadhruv1611@gmail.com>
2024-06-09 05:53:35 +0530

    removed button

--- food-delivery-app/src/App.jsx
+++ food-delivery-app/src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
 
-import Menu from "./components/Menu"
-import Head from "./components/Head"
+import Menu from "./components/Menu";
+import Head from "./components/Head";
 function App() {
   return (
     <>
-    <Head />
-    <Menu />
-
+      <Head />
+      <div className="body">
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+        <Menu />
+      </div>
     </>
-  )    
+  );
 }
-
 
 export default App;
--- food-delivery-app/src/components/Menu.jsx
+++ food-delivery-app/src/components/Menu.jsx
@@ -2,9 +2,21 @@ import "./App.css";
 function Menu() {
   return (
     <div>
-<h1>hello</h1>
+      <div className="card">
+        <img
+          className="food"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/wse52fmwiinpkvueeues"
+          alt="food"
+        />
+        <h3>FOODS</h3>
+        <h3>Biryani, north indian, asian</h3>
+        <h3>4.4 stars</h3>
+        <h3>38 minutes</h3>
+        {/* <button>Add to cart</button> */}
+      </div>
     </div>
-  )
+
+  );
 }
 
-export default Menu
+export default Menu;
